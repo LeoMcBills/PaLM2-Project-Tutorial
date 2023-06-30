bc48cf3e6e86dc435b2e6088b00bd9d1a16d032e
LeoMcBills <leokinyera9@gmail.com>
2023-06-30 12:46:09 +0300

    handle api and display messages

--- app.py
+++ app.py
@@ -25,3 +25,23 @@ with st.form("chat_input", clear_on_submit=True):
         )
 
         b.form_submit_button("send", use_container_width=True)
+
+# Make user input at the left side of the screen, so it looks like a chat app
+for msg in st.session_state.messages:
+    message(msg["content"], is_user=msg["role"] == "user") # display message on the screen
+
+# configure Google's PALM API with your API Key and response from the API
+if user_prompt and palm_api_key:
+    palm.configure(api_key=palm_api_key)
+
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    message(user_prompt, is_user=True)
+
+    response = palm.chat(messages=[user_prompt]) # get response from Google's PaLM API
+
+    msg = {"role": "assistant", "content": response.last}
+
+    st.session_state.messages.append(msg) # add message to the chat history
+
+    message(msg["content"]) # display message on the screen
